bcf555ac65ba73a902fa388f5768fc2aa181ca9e
Krish Patel <krishpatel200508@gmail.com>
2024-02-08 12:24:48 +0530

    Format Javascript

--- Digital-Clock.js
+++ Digital-Clock.js
@@ -1,30 +1,47 @@
 showtime = () => {
-
   // Variable Declaration ---------------------------------------------------------------------------
 
   let date = new Date();
+
   let h = date.getHours();
+
   let m = date.getMinutes();
+
   let s = date.getSeconds();
+
   let session = "AM";
+
   let currentdate = date.getDate();
+
   let currentmonth = date.getMonth();
+
   let currentyear = date.getFullYear();
 
   // Word Month Format ------------------------------------------------------------------------------------------------------
 
   let month = [
     "Jan",
+
     "Feb",
+
     "March",
+
     "April",
+
     "May",
+
     "June",
+
     "July",
+
     "Aug",
+
     "Sept",
+
     "Oct",
+
     "Nov",
+
     "Dec",
   ];
 
@@ -39,17 +56,23 @@ showtime = () => {
   if (h == 0) {
     h = 12; // 12 hour format
   }
+
   if (h > 12) {
     h = h - 12; // 12 hour format  ex :- 13 , h= 13-12 = 1 pm
+
     session = "PM";
   }
 
   // Two-Digit Number Format ---------------------------------------------------------------------------------------
 
   h = h < 10 ? "0" + h : h;
+
   m = m < 10 ? "0" + m : m;
+
   s = s < 10 ? "0" + s : s;
+
   currentdate = currentdate < 10 ? "0" + currentdate : currentdate;
+
   currentmonth = currentmonth < 10 ? "0" + currentmonth : currentmonth;
 
   // Show up Format Pattern ------------------------------------------------------------------------------------------------------------
